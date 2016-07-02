22fa1839bd474bcf9ec37fb12e0f4f8ad7f2304b
Wesam <wesam@Wesams-MacBook-Air.local>
2016-07-02 03:46:41 +0300

    android calculator app **not final **

---
+++ app/src/main/java/com/dothejob/dothe/calculator/MainActivity.java
@@ -0,0 +1,91 @@
+package com.dothejob.dothe.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button zeroBtn ,oneBtn ,towBtn ,threBtn ,foreBtn ,fiveBtn ,sixBtn ,svenBtn ,eightBtn ,nienBtn ,clearBtn ,tempBtn;
+    ImageButton diviedBtn ,multiBut ,subBtn ,addBtn ,calckBtn ,imgTempBtn;
+    TextView clackText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Buttons 0 for 9 and CLEAR
+        //and set the View.OnClickListener for them
+        zeroBtn = (Button) findViewById(R.id.zeroBtn);
+        zeroBtn.setOnClickListener(this);
+        oneBtn = (Button) findViewById(R.id.oneBtn);
+        oneBtn.setOnClickListener(this);
+        towBtn = (Button) findViewById(R.id.towBtn);
+        towBtn.setOnClickListener(this);
+        threBtn = (Button) findViewById(R.id.threBtn);
+        threBtn.setOnClickListener(this);
+        foreBtn = (Button) findViewById(R.id.foreBtn);
+        foreBtn.setOnClickListener(this);
+        fiveBtn = (Button) findViewById(R.id.fiveBtn);
+        fiveBtn.setOnClickListener(this);
+        sixBtn = (Button) findViewById(R.id.sixBtn);
+        sixBtn.setOnClickListener(this);
+        svenBtn = (Button) findViewById(R.id.svenBtn);
+        svenBtn.setOnClickListener(this);
+        eightBtn = (Button) findViewById(R.id.eightBtn);
+        eightBtn.setOnClickListener(this);
+        nienBtn = (Button) findViewById(R.id.nienBtn);
+        nienBtn.setOnClickListener(this);
+        clearBtn = (Button) findViewById(R.id.clearBtn);
+        clearBtn.setOnClickListener(this);
+
+        //ImageButton for = + - * /
+        //and set the View.OnClickListener for them
+        diviedBtn = (ImageButton) findViewById(R.id.diviedBtn);
+        diviedBtn.setOnClickListener(this);
+        multiBut = (ImageButton) findViewById(R.id.multiBut);
+        multiBut.setOnClickListener(this);
+        subBtn = (ImageButton) findViewById(R.id.subBtn);
+        subBtn.setOnClickListener(this);
+        addBtn = (ImageButton) findViewById(R.id.addBtn);
+        addBtn.setOnClickListener(this);
+        calckBtn = (ImageButton) findViewById(R.id.calckBtn);
+        calckBtn.setOnClickListener(this);
+
+        //TextView for Resault
+        clackText = (TextView) findViewById(R.id.calckText);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+
+
+        if (view instanceof Button) {
+            tempBtn = (Button) view;
+            Toast.makeText(this , tempBtn.getText() ,Toast.LENGTH_LONG).show();
+        }
+
+        if (view instanceof ImageButton) {
+            imgTempBtn = (ImageButton) view;
+            if (view == diviedBtn){
+                Toast.makeText(this , "/" ,Toast.LENGTH_LONG).show();
+            }else  if (view == multiBut){
+                Toast.makeText(this , "*" ,Toast.LENGTH_LONG).show();
+            }else if (view == subBtn){
+                Toast.makeText(this , "-" ,Toast.LENGTH_LONG).show();
+            }else if (view == addBtn){
+                Toast.makeText(this , "+" ,Toast.LENGTH_LONG).show();
+            }else if (view == calckBtn){
+                Toast.makeText(this , "=" ,Toast.LENGTH_LONG).show();
+            }
+
+        }
+
+    }
+}
